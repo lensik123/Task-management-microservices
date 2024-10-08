2cb4d694d5f1acae5813dee9e017148205a9a4dc
Turpal Baysarov <turpal.baysarov@bk.ru>
2024-10-08 15:42:05 +0300

    commit

---
+++ statistic/src/main/java/ru/baysarov/statistic/TaskStatisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.baysarov.statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskStatisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskStatisticsServiceApplication.class, args);
+	}
+
+}
