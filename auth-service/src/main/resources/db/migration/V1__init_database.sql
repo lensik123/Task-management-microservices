3332c84c545bce429d671edef77b23f9df12f129
Turpal Baysarov <133017675+lensik123@users.noreply.github.com>
2024-10-18 12:16:19 +0300

    Update V1__init_database.sql

--- auth-service/src/main/resources/db/migration/V1__init_database.sql
+++ auth-service/src/main/resources/db/migration/V1__init_database.sql
@@ -5,5 +5,5 @@ CREATE TABLE users
     last_name  VARCHAR(100) NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
-    role       VARCHAR(50) NOT NULL -- Обновлено для трех ролей
+    role       VARCHAR(50) NOT NULL 
 );
